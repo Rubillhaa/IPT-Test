df9189e7bbeb30a98a547b2af24d8f8097553b41
Rubillhaa <124807717+Rubillhaa@users.noreply.github.com>
2023-04-19 15:42:13 +0800

    Revert "code for Rubillhaa"

--- sample.c
+++ sample.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main() {
-
-	printf("Hello Wolrd in Git!");		
-	printf("I am charlesss");
 	printf("Hello Wolrd in Git!");
 	printf("Neil Alfred Javerle");
 	printf("Adi Estrella");		
